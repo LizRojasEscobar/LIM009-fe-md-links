59c6207106912091742736d7f558e6d49bbdd3b7
LizRojasEscobar <lizrojasescobar19@gmail.com>
2019-06-19 22:01:10 -0500

    refactorizando funcion MdlinksCli y test terminado

--- src/index.js
+++ src/index.js
@@ -1,9 +1,69 @@
 #!/usr/bin/env node
 import {mdLinks} from './api-mdlinks.js'
 import {getStatusOfLinksForCli} from './cli.js'
+/*
+const [, , ...params] = process.argv;
+const [route, ...opts] = params;*/
+
+export const mdLinksForCli = ( route, options1, options2 )=>{
+  if (route!==undefined && options1 == undefined &&  options2 == undefined) {
+    return mdLinks(route, {validate: false})
+    .then(result => {
+      const newArray = result.map(element => {
+     //   console.log(`${element.href} ${element.text}${element.file} \n`);
+      return  `${element.href} ${element.text} ${element.file}`
+      })
+      const newArrayWithoutValidate = newArray.toString().replace(/,/g,'\n');
+      return newArrayWithoutValidate
+    }).catch(console.error);
+} else if (route!== undefined && options1== '--validate' && options2 ==undefined ){
+   return mdLinks(route, {validate: true})
+    .then(result => {
+      const newArray = result.map(element => {
+       // console.log(`${element.href}${element.text}${element.file}${element.ok} ${element.status}\n`);
+      return `${element.href} ${element.text} ${element.file} ${element.ok} ${element.status}`
+      });
+      const newArrayWithValidate = newArray.toString().replace(/,/g,'\n');
+      return newArrayWithValidate;
+    })
+    .catch(console.error);
+} else if (route!== undefined && options1=='--stats' && options2 ==undefined){
+  
+  return mdLinks(route, {validate: true})
+  .then(result1 => getStatusOfLinksForCli(result1))
+      .then(status => { 
+      return `Total: ${status.total}\nUnique: ${status.unique}`
+         // console.log(`Total: ${status.total} \nUnique: ${status.unique}\n `);
+        })
+    .catch(console.error)
+} else if (route!== undefined && options1=='--validate' && options2 =='--stats' ){
+  return mdLinks(route, {validate: true})
+  .then(result1 => getStatusOfLinksForCli(result1))
+      .then(status => { 
+        return `Total: ${status.total}\nUnique: ${status.unique}\nBroken: ${status.broken}`
+        // console.log(`Total: ${status.total} \nUnique: ${status.unique} \nBroken: ${status.broken}\n`);
+        })
+    .catch(console.error)
+} else{
+  return mdLinks(route, {validate: true})
+  .then(result1 => getStatusOfLinksForCli(result1))
+      .then(status => { 
+        return `Total: ${status.total}\nUnique: ${status.unique}\nBroken: ${status.broken}`
+        // console.log(`Total: ${status.total} \nUnique: ${status.unique} \nBroken: ${status.broken}\n`);
+        })
+    .catch(console.error)
+}
+}
+
+mdLinksForCli('/home/liz/Documentos/md.links/LIM009-fe-md-links/prueba','--validate','--stats').then(result =>{
+  console.log (result)
+});
+/*
+console.log(`Current directory: ${process.cwd()}`);
+path.join(`Current directory: ${process.cwd()}`);
 
 const options = {
-  validate: false, 
+ validate: false, 
   stats:false
 }
 const [, , ...params] = process.argv;
@@ -23,10 +83,10 @@ opts.forEach((element) => {
 });
 
 if (!options.validate && !options.stats) {
-  mdLinks(route, {validate: false})
+  mdLinks(route,options)
     .then(result => {
       result.forEach(element => {
-        console.log(`${element.href} ${element.text.substring(0,50)}${element.file} \n`);
+        console.log(`${element.href} ${element.text}${element.file} \n`);
       });
     })
     .catch(console.error);
@@ -35,14 +95,14 @@ if (options.validate && !options.stats) {
   mdLinks(route, options)
     .then(result => {
       result.forEach(element => {
-        console.log(`${element.href} ${element.text.substring(0,50)}${element.file} ${element.ok} ${element.status}\n`);
+        console.log(`${element.href}${element.text}${element.file}${element.ok} ${element.status}\n`);
       });
     })
     .catch(console.error);
 }
 
 if (options.stats && !options.validate) {
-  mdLinks(route, {validate: true})
+  mdLinks(route, options)
   .then(result1 => getStatusOfLinksForCli(result1))
       .then(status => { 
           console.log(`Total: ${status.total} \nUnique: ${status.unique}\n `);
@@ -51,7 +111,7 @@ if (options.stats && !options.validate) {
 };
 
 if (options.stats && options.validate ) {
-  mdLinks(route, {validate: true})
+  mdLinks(route, options)
   .then(result1 => getStatusOfLinksForCli(result1))
       .then(status => { 
           console.log(`Total: ${status.total} \nUnique: ${status.unique} \nBroken: ${status.broken}\n`);
@@ -59,7 +119,4 @@ if (options.stats && options.validate ) {
     .catch(console.error)
 }
 
-/*
-console.log(`Current directory: ${process.cwd()}`);
-path.join(`Current directory: ${process.cwd()}`);
 */
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,24 +1,15 @@
 
-  import {
-    convertToAbsolute,
-    verifyExtension} 
-  from '../src/path-controller.js';
-   import{
-    verifyFile
-   }from '../src/file-controller.js';
-  import{    
-    pathFilesAndDirectories,
-    readFileInside
-  } from '../src/directory-controller.js'
-  import{ 
-    markdownLinkExtractor,
-    getStatusOfLInk
-  } from '../src/links-controller.js'
-  import{
-    mdLinks
-  } from '../src/api-mdlinks.js'
-  import{
-  getStatusOfLinksForCli  } from '../src/cli.js'
+  import mock from 'mock-fs';
+  import {convertToAbsolute,verifyExtension} from '../src/path-controller.js';
+  import{verifyFile }from '../src/file-controller.js';
+  import{pathFilesAndDirectories,readFileInside} from '../src/directory-controller.js'
+  import{markdownLinkExtractor,getStatusOfLInk } from '../src/links-controller.js'
+  import{mdLinks} from '../src/api-mdlinks.js'
+  import{getStatusOfLinksForCli} from '../src/cli.js'
+  import{mdLinksForCli} from '../src/index.js'
+  import{path} from 'path'
+
+
 
 describe('Deberia retornar que el path sea absoluto',() =>{
   it ('deberia ser una funcion', ()=>{
@@ -193,6 +184,47 @@ describe('getStatusOfLinksForCli', ()=>{
      '/home/liz/Documentos/md.links/LIM009-fe-md-links/prueba/README.md',
     status: 200,
     ok: 'ok' } ])).toStrictEqual(  {"broken": 1, "total": 2, "unique": 2})
-
   })
+ })
+
+ describe('mdLinksForCli', ()=>{
+   it('deberia de ser una funcion', ()=>{
+     expect(typeof mdLinksForCli).toBe('function')
+   })
+   it('deberia de retornar solo href, text y file',(done)=>{
+    mdLinksForCli('/home/liz/Documentos/md.links/LIM009-fe-md-links/prueba').then(result=>{
+      const newString = 
+      `https://nodejs.org/es/123456789 Node.js /home/liz/Documentos/md.links/LIM009-fe-md-links/prueba/README.md\nhttps://developers.google.com/v8/ motor de JavaScript V8 de Chrome /home/liz/Documentos/md.links/LIM009-fe-md-links/prueba/README.md\nhttps://es.wikipedia.org/wiki/Markdown Markdown /home/liz/Documentos/md.links/LIM009-fe-md-links/prueba/prueba1/README.md`;
+    expect(result).toEqual(newString) 
+    done()
+    })
+   })
+   it('deberia de retornar solo href, text y file, status y ok',(done)=>{
+    mdLinksForCli('/home/liz/Documentos/md.links/LIM009-fe-md-links/prueba','--validate').then(result=>{
+      const newString1 = `https://nodejs.org/es/123456789 Node.js /home/liz/Documentos/md.links/LIM009-fe-md-links/prueba/README.md fail 404\nhttps://developers.google.com/v8/ motor de JavaScript V8 de Chrome /home/liz/Documentos/md.links/LIM009-fe-md-links/prueba/README.md ok 200\nhttps://es.wikipedia.org/wiki/Markdown Markdown /home/liz/Documentos/md.links/LIM009-fe-md-links/prueba/prueba1/README.md ok 200`;
+    expect(result).toEqual(newString1) 
+    done();
+    })
+   })
+   it('deberia de retornar solo href, text y file, status y ok',(done)=>{
+    mdLinksForCli('/home/liz/Documentos/md.links/LIM009-fe-md-links/prueba','--stats').then(result=>{
+      const newString1 = `Total: 3\nUnique: 3`
+    expect(result).toEqual(newString1) 
+    done();
+    })
+   })
+   it('deberia de retornar solo href, text y file, status y ok',(done)=>{
+    mdLinksForCli('/home/liz/Documentos/md.links/LIM009-fe-md-links/prueba','--validate', '--stats').then(result=>{
+      const newString1 = `Total: 3\nUnique: 3\nBroken: 1`
+    expect(result).toEqual(newString1) 
+    done();
+    })
+   })
+   it('deberia de retornar solo href, text y file, status y ok',(done)=>{
+    mdLinksForCli('/home/liz/Documentos/md.links/LIM009-fe-md-links/prueba','--stats', '--validate').then(result=>{
+      const newString1 = `Total: 3\nUnique: 3\nBroken: 1`
+    expect(result).toEqual(newString1) 
+    done();
+    })
+   })
  })
